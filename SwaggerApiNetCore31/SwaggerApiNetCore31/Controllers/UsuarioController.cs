cae5d31ba9c7c4b9843b569506c75d9a8e3b8c03
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2020-10-07 22:27:09 -0300

    Segundo Commit.
    Ajustando chamada da API atraves do swagger

---
+++ SwaggerApiNetCore31/SwaggerApiNetCore31/Controllers/UsuarioController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwaggerApiNetCore31.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace SwaggerApiNetCore31.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        /// <summary>
+        /// Obter todos os usuários.
+        /// </summary>
+        /// <response code="200">A lista de usuários foi obtida com sucesso.</response>
+        /// <response code="500">Ocorreu um erro ao obter a lista de usuários.</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<UsuarioViewModel>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult Get()
+        {
+            try
+            {
+                var usuarios = ObterUsuarios();
+                return Ok(usuarios);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Obter um usuário específico por ID.
+        /// </summary>
+        /// <param name="id">ID do usuário.</param>
+        /// <response code="200">O usuário foi obtido com sucesso.</response>
+        /// <response code="404">Não foi encontrado usuário com ID especificado.</response>
+        /// <response code="500">Ocorreu um erro ao obter o usuário.</response>
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                var usuarios = ObterUsuarios();
+                var usuario = usuarios.FirstOrDefault(x => x.Id.Equals(id));
+
+                if (usuario == null)
+                    return NotFound(new { message = "Usuário não encontrado." });
+
+                return Ok(usuario);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Cadastrar usuário.
+        /// </summary>
+        /// <param name="usuario">Modelo do usuário.</param>
+        /// <response code="200">O usuário foi cadastrado com sucesso.</response>
+        /// <response code="400">O modelo do usuário enviado é inválido.</response>
+        /// <response code="500">Ocorreu um erro ao cadastrar o usuário.</response>
+        [HttpPost]
+        public IActionResult Post(UsuarioViewModel usuario)
+        {
+            try
+            {
+                //Salva o usuário na base de dados.
+                return StatusCode((int)HttpStatusCode.Created);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Alterar usuário.
+        /// </summary> 
+        /// <param name="id">ID do usuário.</param>
+        /// <param name="usuario">Modelo do usuário.</param>
+        /// <response code="200">O usuário foi alterado com sucesso.</response>
+        /// <response code="400">O modelo do usuário enviado é inválido.</response>
+        /// <response code="404">Não foi encontrado usuário com ID especificado.</response>
+        /// <response code="500">Ocorreu um erro ao alterar o usuário.</response>
+        [HttpPut("{id}")]
+        public IActionResult Put([FromRoute]Guid id, [FromBody] UsuarioViewModel usuario)
+        {
+            try
+            {
+                var usuarios = ObterUsuarios();
+                var usuarioFakeDb = usuarios.FirstOrDefault(x => x.Id.Equals(id));
+
+                if (usuarioFakeDb == null)
+                    return NotFound(new { message = "Usuário não encontrado." });
+
+                //Atualiza o usuário na base de dados.
+
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Deletar usuário.
+        /// </summary>
+        /// <param name="id">ID do usuário.</param>
+        /// <response code="200">O usuário foi deletado com sucesso.</response>
+        /// <response code="404">Não foi encontrado usuário com ID especificado.</response>
+        /// <response code="500">Ocorreu um erro ao deletar o usuário.</response>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                var usuarios = ObterUsuarios();
+                var ususario = usuarios.FirstOrDefault(x => x.Id.Equals(id));
+
+                if (ususario == null)
+                    return NotFound(new { message = "Usuário não encontrado." });
+
+                //Exclui o usuário na base de dados.
+
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private IEnumerable<UsuarioViewModel> ObterUsuarios()
+        {
+            return new List<UsuarioViewModel>
+            {
+                new UsuarioViewModel { Id = Guid.Parse("CAEF4812-598E-4473-8EFC-B338AF69A18F"), Name = "João", Email = "joao@outlook.com" },
+                new UsuarioViewModel { Id = Guid.Parse("B88DCCAF-A3A2-436F-8B09-27F0E1F73321"), Name = "José", Email = "jose@outlook.com" },
+                new UsuarioViewModel { Id = Guid.Parse("1E018A4E-FC1B-4E2D-91CA-8076D60CF63F"), Name = "Maria", Email = "maria@outlook.com" }
+            };
+        }
+    }
+}
---
+++ SwaggerApiNetCore31/SwaggerApiNetCore31/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerApiNetCore31
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Empresa X", Version = "v1", });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            });
+        }
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }            
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }
+}
